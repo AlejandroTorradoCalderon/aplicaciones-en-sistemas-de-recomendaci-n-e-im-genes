698e5aca1c850f61b72f42724ffaa77c32fa627a
AlejandroTorradoCalderon <112442070+AlejandroTorradoCalderon@users.noreply.github.com>
2025-07-06 21:20:05 -0500

    Update app_vf.py

--- app_vf.py
+++ app_vf.py
@@ -77,7 +77,7 @@ def load_image_model():
     # Solo construir (esto se cachea)
     model = build_image_model()
     # La carga se hace fuera del cache
-    state_dict = torch.load("best_model.pth", map_location=device, weights_only=False)
+    state_dict = torch.load('best_model_v2.pth', map_location=device)
     model.load_state_dict(state_dict)
     model.to(device)
     model.eval()
